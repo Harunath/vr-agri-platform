d96870878b00ec7bba22996cf5b9dc901312f64a
harunath <harunath04@gmail.com>
2025-07-26 15:46:55 +0530

    first commit

---
+++ apps/vr-user-app/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+	return (
+		<main className=" min-h-screen w-screen flex justify-center items-center">
+			<h1 className="text-8xl">VR USER APP</h1>
+		</main>
+	);
+}
